81ce2178720b60340f0532b1361a5cf90aae5227
priyankagupta34 <34priyankagupta@gmail.com>
2022-08-15 19:58:59 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="style.css">
+</head>
+ <body>
+   <div class="wrapper">
+     <div class="flag">
+       <div class="wave"></div>
+       <div class="top"></div>
+       <div class="middle">
+         <div class="wheel">
+           <span class="line"></span>
+           <span class="line"></span>
+           <span class="line"></span>
+           <span class="line"></span>
+           <span class="line"></span>
+           <span class="line"></span>
+           <span class="line"></span>
+           <span class="line"></span>
+           <span class="line"></span>
+           <span class="line"></span>
+           <span class="line"></span>
+           <span class="line"></span>
+         </div>
+       </div>
+       <div class="bottom"></div>
+      </div>
+     </div>
+ </body>
+</html>
+`;
